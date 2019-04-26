3ffb17631b5d7259254424f847a6a3d6829a4010
juan ocampo <juan.ocampo@globant.com>
2019-04-25 23:00:25 -0500

    Setting up ViewModel factory

--- app/src/main/java/juanocampo/myapplication/com/viewmodel/MovieViewModelFactory.kt
+++ app/src/main/java/juanocampo/myapplication/com/viewmodel/MovieViewModelFactory.kt
@@ -1,4 +1,13 @@
 package juanocampo.myapplication.com.viewmodel
 
-class MovieViewModelFactory {
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import juanocampo.myapplication.com.model.IRepository
+
+class MovieViewModelFactory(private val repository: IRepository) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (MovieViewModel::class.java.isAssignableFrom(modelClass)) return MovieViewModel(repository) as T
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
